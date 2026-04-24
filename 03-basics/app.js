263f35d373adf5602b7d9615c56d0e57c2bc5d60
Abolhasan Ashori <abolhasan.ashori@gmail.com>
2026-04-24 13:47:29 +0330

    feat: send server response

--- 03-basics/app.js
+++ 03-basics/app.js
@@ -2,6 +2,13 @@ const http = require("node:http");
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    //process.exit();
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Hello from node.js server</h1></body>");
+    res.write("</html>");
+    res.end();
 });
 
 server.listen(3000);
