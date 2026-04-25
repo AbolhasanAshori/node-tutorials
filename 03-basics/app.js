f4888442493e4d40951b0fa2e418ae66792f56e1
Abolhasan Ashori <abolhasan.ashori@gmail.com>
2026-04-25 09:08:18 +0330

    fix: stop the response for the / route

--- 03-basics/app.js
+++ 03-basics/app.js
@@ -1,14 +1,22 @@
 const http = require("node:http");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    //process.exit();
-    res.setHeader("Content-Type", "text/html");
+  const { url } = req;
+
+  if (url === '/') {
     res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body><h1>Hello from node.js server</h1></body>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write("<body><form action=\"/message\" method=\"POST\"><input type=\"text\"><button type=\"submit\">Send</button></form></body>");
     res.write("</html>");
-    res.end();
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from node.js server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
