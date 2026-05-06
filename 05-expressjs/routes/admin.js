f9467e915c083e7fa1b25fa5cf9744f72440517b
Abolhassan Ashoori <abolhasan.ashori@gmail.com>
2026-05-06 20:39:34 +0330

    fix: replace ../ with .. to avoid platform-specific delimiter

--- 05-expressjs/routes/admin.js
+++ 05-expressjs/routes/admin.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router
   .get("/add-product", (_req, res) => {
-    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+    res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
   })
   .post("/add-product", (req, res) => {
     console.log(req.body);
