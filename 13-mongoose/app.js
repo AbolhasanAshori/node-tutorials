1cbbe683e33f45c85ec6e498101c8c99851429a9
Abolhassan Ashoori <abolhasan.ashori@gmail.com>
2026-05-20 10:18:39 +0330

    feat(api)!: re-implement user model & authenticate dummy current user
    Changes:
    - Seed one dummy authenticated user

--- 13-mongoose/app.js
+++ 13-mongoose/app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const { getNotFound } = require("./controllers/error");
 const { adminRoutes, shopRoutes } = require("./routes");
-const { default: mongoose } = require("mongoose");
+const { default: mongoose, Types } = require("mongoose");
 const { createDbConnectionUri } = require("./util/database");
+const User = require("./models/user");
 
 const app = express();
 app.engine(
@@ -26,18 +27,14 @@ app.set("views", "views");
 app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, _res, next) => {
-//   User.findById("5baa2528563f16379fc8a610")
-//     .then((user) => {
-//       req.user = new User({
-//         ...user,
-//         username: user.name,
-//         id: user._id.toString(),
-//       });
-//       next();
-//     })
-//     .catch(console.error);
-// });
+app.use((req, _res, next) => {
+  User.findById("5baa2528563f16379fc8a610")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch(console.error);
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -55,6 +52,14 @@ mongoose
   )
   .then(() => {
     console.log("Successfully connected to MongoDB using Mongoose!");
+
+    return User.updateOne(
+      { _id: new Types.ObjectId("5baa2528563f16379fc8a610") },
+      { $setOnInsert: { name: "test", email: "test@email.com" } },
+      { upsert: true },
+    );
+  })
+  .then(() => {
     app.listen(3000, () => {
       console.log("Server running at http://localhost:3000");
     });
