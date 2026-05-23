6c1322d58fc368e2f93292ea62c93c86183c9364
Abolhassan Ashoori <abolhasan.ashori@gmail.com>
2026-05-23 16:31:27 +0330

    refactor: improve app.js readability

--- 15-authentication/app.js
+++ 15-authentication/app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const path = require("node:path");
 const express = require("express");
 const { engine } = require("express-handlebars");
@@ -19,55 +20,45 @@ const MONGODB_URI = createDbConnectionUri({
   dbType: process.env.DB_TYPE ?? "mongodb",
 });
 
-const app = express();
-const store = MongoStore.create({
-  mongoUrl: MONGODB_URI,
-  collectionName: "sessions",
-  ttl: 1000 * 60 * 60 * 24 * 14,
+const viewEngine = engine({
+  layoutsDir: "views/layouts",
+  partialsDir: "views/partials",
+  defaultLayout: "main-layout",
+  extname: "hbs",
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+  },
+});
+const sessionHandler = session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: MONGODB_URI,
+    collectionName: "sessions",
+    ttl: 1000 * 60 * 60 * 24 * 14,
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 14,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  },
 });
 const csrfProtection = csrf();
 
-app.engine(
-  "hbs",
-  engine({
-    layoutsDir: "views/layouts",
-    partialsDir: "views/partials",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-    runtimeOptions: {
-      allowProtoPropertiesByDefault: true,
-    },
-  }),
-);
-app.set("view engine", "hbs");
-app.set("views", "views");
+const app = express().engine("hbs", viewEngine).set("view engine", "hbs").set("views", "views");
 
-app.use(express.urlencoded());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(cookieParser());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 14,
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-    },
-  }),
-);
-app.use(csrfProtection);
-app.use(authenticateUser);
-app.use(populateLocals);
+app
+  .use(express.urlencoded())
+  .use(express.static(path.join(__dirname, "public")))
+  .use(cookieParser())
+  .use(sessionHandler)
+  .use(csrfProtection)
+  .use(authenticateUser)
+  .use(populateLocals);
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
-app.use(authRoutes);
-
-app.use(getNotFound);
+app.use("/admin", adminRoutes).use(shopRoutes).use(authRoutes).use(getNotFound);
 
 mongoose
   .connect(MONGODB_URI)
